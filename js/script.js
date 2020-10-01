9ea3e7a43d14f7454f16abde727eb19817e3ccda
amtaneja <68820543+amtaneja@users.noreply.github.com>
2020-10-01 22:29:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+
+
+   
