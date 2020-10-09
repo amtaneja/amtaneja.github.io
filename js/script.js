d3ca4418a5299e4710c00be4c12f11c502b3dad7
amtaneja <68820543+amtaneja@users.noreply.github.com>
2020-10-09 23:22:04 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const cursor = document.querySelector("#cursor"),
+  anchor = document.querySelectorAll("a");
+  
+
+//   custom cursor
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.pageX,
+    y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
+
+// anchor tag hover effect
+
+anchor.forEach((anc) => {
+  anc.addEventListener("mouseover", () => {
+    cursor.style.transform = "scale(7)";
+    cursor.style.animationName = "borderAnim";
+  });
+  anc.addEventListener("mouseleave", () => {
+    cursor.style.transform = "";
+    cursor.style.animationName = "";
+  });
+});
+
+
+
+   
