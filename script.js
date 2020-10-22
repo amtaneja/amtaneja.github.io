107693845d3c5744253af22407c63c49620220bf
amantaneja31 <68807160+amantaneja31@users.noreply.github.com>
2020-10-22 19:17:52 +0530

    1

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ sr.reveal('.animate-right',{origin:'right',duration:1200,distance:'25rem',delay:
 sr.reveal('.animate-top',{origin:'top',duration:1000,distance:'25rem',delay:300})
 sr.reveal('.animate-bottom',{origin:'bottom',duration:1000,distance:'25rem',delay:300})
 $(document).ready(function(){$(".menu a").click(function(){$(".toggler").prop("checked",false);$(".toggler").prop("overflow-y",hidden);});});$(document).ready(function(){$(window).scroll(function(){if($(window).scrollTop()>300){$('#up').css({"opacity":"1","pointer-events":"auto"});}else{$('#up').css({"opacity":"0","pointer-events":"none"});}});$('#up').click(function(){$('html').animate({scrollTop:0},500);});});
+
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+    });
