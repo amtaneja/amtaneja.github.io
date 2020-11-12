ccfe3fb67ccbed782d0c28611281dd5e4d276f04
amantaneja31 <68807160+amantaneja31@users.noreply.github.com>
2020-11-12 13:43:47 +0530

    c

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
+const tl = gsap.timeline({ default: { ease: "power1.out" } });
+
+tl.to(".loader", { y: "0%", duration: 0.7, stagger: 0.25 });
+tl.to(".loading", { y: "-200%", duration: 1, delay: 1.5 });
+
+
 /*-----------animation on scroll-----------*/
 window.sr = ScrollReveal({ reset: true });
 sr.reveal(".animate-left", {
-  origin: "left",
-  duration: 1200,
-  distance: "25rem",
-  delay: 300,
+    origin: "left",
+    duration: 1200,
+    distance: "25rem",
+    delay: 300,
 });
 sr.reveal(".animate-right", {
-  origin: "right",
-  duration: 1200,
-  distance: "25rem",
-  delay: 300,
+    origin: "right",
+    duration: 1200,
+    distance: "25rem",
+    delay: 300,
 });
 sr.reveal(".animate-top", {
-  origin: "top",
-  duration: 1000,
-  distance: "25rem",
-  delay: 300,
+    origin: "top",
+    duration: 1000,
+    distance: "20rem",
+    delay: 200,
 });
 sr.reveal(".animate-bottom", {
-  origin: "bottom",
-  duration: 1000,
-  distance: "25rem",
-  delay: 300,
+    origin: "bottom",
+    duration: 1000,
+    distance: "20rem",
+    delay: 200,
 });
 
 /*-----------menu closed on back-----------*/
-$(document).ready(function () {
-  $(".menu a").click(function () {
-    $(".toggler").prop("checked", false);
-    $(".toggler").prop("overflow-y", hidden);
-  });
+
+$(document).ready(function() {
+    $(".menu a").click(function() {
+        $(".toggler").prop("checked", false);
+    });
 });
 
 /*-----------arrow up icon-----------*/
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-      $("#up").css({ opacity: "1", "pointer-events": "auto" });
-    } else {
-      $("#up").css({ opacity: "0", "pointer-events": "none" });
-    }
-  });
-  $("#up").click(function () {
-    $("html").animate({ scrollTop: 0 }, 500);
-  });
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            $("#up").css({ opacity: "1", "pointer-events": "auto" });
+        } else {
+            $("#up").css({ opacity: "0", "pointer-events": "none" });
+        }
+    });
+    $("#up").click(function() {
+        $("html").animate({ scrollTop: 0 }, 500);
+    });
 });
 
 
 /*---------loading animation-----------*/
 
-setTimeout(function () {
-  $(".loading").fadeToggle();
-}, 1800);
 
 /*---------fixed menu-----------*/
 function lockScroll() {
-  $("body").toggleClass("fixedPosition");
+    $("body").toggleClass("fixedPosition");
 }
